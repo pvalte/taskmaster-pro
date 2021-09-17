e24316d7e1189b0867fe2d6a9986e5f19fd59b42
Paige A <pvaltemare@gmail.com>
2021-09-16 21:50:24 -0700

    Font and custom css UX improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,7 +158,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -194,18 +194,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  // activate: function (event) {
-  //   console.log("activate", this);
-  // },
-  // deactivate: function (event) {
-  //   console.log("deactivate", this);
-  // },
-  // over: function (event) {
-  //   console.log("over", event.target);
-  // },
-  // out: function (event) {
-  //   console.log("out", event.target);
-  // },
+  activate: function (event) {
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
+  },
+  deactivate: function (event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
+  },
+  over: function (event) {
+    $(event.target).addClass('dropover-active');
+    $('.bottom-trash').addClass('bottom-trash-active');
+  },
+  out: function (event) {
+    $(event.target).removeClass('dropover-active');
+    $('.bottom-trash').removeClass('bottom-trash-active');
+  },
   update: function (event) {
     // array to store the task data in
     var tempArr = [];
